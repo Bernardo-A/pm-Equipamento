c37525ef8c97313057f228751742a8250f84eaed
BernardoAgrelos <bernardo.agrelos@edu.unirio.br>
2023-07-20 13:37:10 -0300

    correcoes

--- Equipamento.Tests/TrancaServiceTest.cs
+++ Equipamento.Tests/TrancaServiceTest.cs
@@ -3,6 +3,7 @@ using Equipamento.API.Services;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,12 +15,12 @@ namespace Equipamento.Tests
     {
         private readonly Mock<IBicicletaService> _bicicletaService = new();
 
-        private readonly Mock <ITotemService> _totemService = new();
+        private readonly Mock<ITotemService> _totemService = new();
 
         [Fact]
         public void CreateOnSuccessReturnTrancaModel()
         {
-            var sut = new TrancaService(_bicicletaService.Object, _totemService.Object );
+            var sut = new TrancaService(_bicicletaService.Object, _totemService.Object);
 
             var result = sut.CreateTranca(new TrancaDto());
 
@@ -122,10 +123,61 @@ namespace Equipamento.Tests
 
             var sut = new TrancaService(_bicicletaService.Object, mockTotemService.Object);
 
-            var result = sut.GetBicicleta(It.IsAny<int>());
+            var result = sut.GetTotem(It.IsAny<int>());
 
             Assert.NotNull(result);
             Assert.Equal(typeof(TotemModel), result.GetType());
+        }
+
+        [Fact]
+        public void TrancaModelTest()
+        {
+            var tranca = new TrancaRedeDto
+            {
+                FuncionarioId = 0,
+                TotemId = 0,
+                TrancaId = 0,
+            };
+            Assert.Equal(typeof(TrancaRedeDto), tranca.GetType());
+        }
+
+        [Fact]
+        public void ChangeStatusTest()
+        {
+            var sut = new TrancaService(_bicicletaService.Object, _totemService.Object);
+
+            sut.CreateTranca(new TrancaDto());
+
+            var result = sut.ChangeStatus(It.IsAny<int>(), It.IsAny<string>());
+
+            Assert.Equal(typeof(TrancaModel), result.GetType());
+        }
+
+        [Fact]
+        public async void GetBicicletaTest()
+        {
+            var sut = new TrancaService(_bicicletaService.Object, _totemService.Object);
+
+            sut.CreateTranca(new TrancaDto());
+
+            await sut.AddBicicletaToTranca(new BicicletaRedeDto());
+
+            var result = sut.GetBicicleta(It.IsAny<int>());
+
+            Assert.Equal(typeof(BicicletaModel), result?.GetType());
+
+        }
+
+        [Fact]
+        public void LockTest()
+        {
+            var mockBicicletaService = new Mock<IBicicletaService>();
+            mockBicicletaService.Setup(service => service.GetBicicleta(It.IsAny<int>())).Returns(new BicicletaModel());
+
+            var sut = new TrancaService(_bicicletaService.Object, _totemService.Object);
+            var result = sut.Lock(It.IsAny<int>(), It.IsAny<int>());
+
+            Assert.Equal(typeof(BicicletaModel), result.GetType());
         }
     }
 }
